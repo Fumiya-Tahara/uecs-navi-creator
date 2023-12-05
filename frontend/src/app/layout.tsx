11b23810304dc1892ae070d2bcd1d67cac40954f
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-12-05 12:50:56 +0900

    add google icons

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
