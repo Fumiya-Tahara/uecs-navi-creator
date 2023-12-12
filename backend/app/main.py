6c474da3960f8ae062eb05e7f5a3da8ced35c974
ST <st20027802@gmail.com>
2023-12-12 13:58:59 +0900

    edited main.py and defined some functions to operate data.

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,17 +3,19 @@ from sqlalchemy.orm import Session
 from app.database.settings import SessionLocal
 from app.database.models import Uecs, Parameter
 from app.schemas import UecsCreator, ParameterCreator
-# from app.database.settings import Base
+from datetime import time, timedelta
 
 app = FastAPI()
 
-# Dependency
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+# CRUD定義
 
 def create_uecs(db: Session, uecs_data: UecsCreator):
     db_uecs = Uecs(**uecs_data.dict())
@@ -29,11 +31,12 @@ def create_parameter(db: Session, parameter_data: ParameterCreator):
     db.refresh(db_parameter)
     return db_parameter
 
-# API endpoints
-@app.post("/uecs/", response_model=UecsCreator)
+
+# 実際のCRUD操作(仮)
+@app.post("/uecs/create", response_model=UecsCreator)
 def create_uecs_endpoint(uecs_data: UecsCreator, db: Session = Depends(get_db)):
     return create_uecs(db, uecs_data)
 
-@app.post("/parameters/", response_model=ParameterCreator)
+@app.post("/parameters/create", response_model=ParameterCreator)
 def create_parameter_endpoint(parameter_data: ParameterCreator, db: Session = Depends(get_db)):
     return create_parameter(db, parameter_data)
