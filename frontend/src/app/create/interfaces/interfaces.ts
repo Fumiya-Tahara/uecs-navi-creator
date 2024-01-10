e6792eb7b540772346ea1fe38759b286992edf5a
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-01-10 10:10:06 +0900

    set state from add device button

---
+++ frontend/src/app/create/interfaces/interfaces.ts
@@ -0,0 +1,12 @@
+export interface Device {
+  id: number;
+  name: string;
+  controllParam: string;
+  unit: string;
+}
+
+export interface Sensor {
+  id: number;
+  name: string;
+  unit: string;
+}
